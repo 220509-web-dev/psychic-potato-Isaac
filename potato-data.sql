6648d98411db36b4dc2adab416db2946e839e3c3
isaac harris <isaachrr4@gmail.com>
2022-05-17 17:20:20 -0400

    created some mock data

---
+++ potato-data.sql
@@ -0,0 +1,12 @@
+set search_path to potato-llc;
+
+-- Create employee roles
+insert into employee_role values ('Project Manager'), ('Project Lead'), ('Senior Developer'),('Junior Developer'), ('Intern'), ('HR Specialist');
+
+-- Create departments data
+insert into departments values ('Management'), ('Developers'), ('Human Resources');
+
+-- Create salary table data
+insert into salaries values ('250,000'),('115,000'),('95,000'),('75,000'), ('65,000'), ('45,000');
+
+-- Create employee data
