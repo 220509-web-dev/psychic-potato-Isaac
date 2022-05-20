f30adb8c2d55c40ba97fb17d6ed160e91064e742
isaac harris <isaachrr4@gmail.com>
2022-05-20 10:09:36 -0400

    made query file

---
+++ query-data.sql
@@ -0,0 +1,12 @@
+--select all employees
+select * from employees;
+
+--select all from salaries
+select * from salaries;
+
+--select all from departments
+select * from departments;
+
+--select all from employee roles
+select * from employee_role;
+
