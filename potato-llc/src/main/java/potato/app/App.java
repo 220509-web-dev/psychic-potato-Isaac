2e0b3d2866496fc67d5492862c5910d31bad5f7b
isaac harris <isaachrr4@gmail.com>
2022-06-06 15:24:21 -0400

    changes

--- potato-llc/src/main/java/potato/app/App.java
+++ potato-llc/src/main/java/potato/app/App.java
@@ -1,8 +1,8 @@
 package potato.app;
 
 import potato.entities.Employees;
-import potato.entities.Departments;
 import potato.entities.Roles;
+import potato.entities.Departments;
 import potato.entities.Salaries;
 
 
